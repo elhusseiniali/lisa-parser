f9ccf45807878820e7a48dfbc80d522950bd15a7
Ali El Husseini <elhusseiniali@icloud.com>
2022-07-13 03:45:20 +0200

    Use predecessors instead of successors for edge analysis.

--- main.py
+++ main.py
@@ -23,53 +23,44 @@ def main(input_file="./data/df3.dot"):
         print("label: ", label)
 
         successors = list(G.successors(x[0]))
+        predecessors = list(G.predecessors(x[0]))
+        print("predecessors: ", predecessors)
+        print("successors: ", successors)
         if "read" in label:
             file_name = re.findall(r"'\s*([^']+?)\s*'", label)
             print(Read(file_name))
-        elif "project" in label:
-            column_name = re.findall(r"'\s*([^']+?)\s*'", label)
-            current_node = Projection(column_name=column_name)
-            print(current_node)
-        elif "assign_to" in label:
-            column_name = re.findall(r"'\s*([^']+?)\s*'", label)
-            current_node = Projection(column_name=column_name)
-            print("&&&&&&&&&&&&&&&")
-            print(current_node)
-            for successor in successors:
-                print((successor, data[successor]))
-                successor_label = clean_str(dict(data[successor])['label'])
-                result = re.findall(r"'\s*([^']+?)\s*'", successor_label)
-                print(result)
-                for name in column_name:
-                    analysis.current["must"].add(name)
-                    analysis.current["may"].add(name)
-                if column_name in result:
-                    print(True)
-                else:
-                    for name in column_name:
-                        analysis.current["must"].add(name)
-                        analysis.current["may"].add(name)
-
-                        analysis.added["may"].add(name)
-                    print(False)
-            print("&&&&&&&&&&&&&&&")
         else:
             column_name = re.findall(r"'\s*([^']+?)\s*'", label)
             if column_name:
                 print("column_name: ", column_name)
-                for name in column_name:
-                    analysis.current["must"].add(name)
-                    analysis.current["may"].add(name)
+                if "project" in label:
+                    column_name = re.findall(r"'\s*([^']+?)\s*'", label)
+                    current_node = Projection(column_name=column_name)
+                    print(current_node)
+                elif "assign_to" in label:
+                    column_name = re.findall(r"'\s*([^']+?)\s*'", label)
+                    current_node = Projection(column_name=column_name)
+                    print("&&&&&&&&&&&&&&&")
+                    print(current_node)
+                    for predecessor in predecessors:
+                        print((predecessor, data[predecessor]))
+                        p_label = clean_str(dict(data[predecessor])['label'])
+                        print("p_label: ", p_label)
+                        result = re.findall(r"'\s*([^']+?)\s*'", p_label)
+                        print("result: ", result)
+                        for name in column_name:
+                            analysis.current["must"].add(name)
+                            analysis.current["may"].add(name)
+                        if not any(i in column_name for i in result):
+                            for name in column_name:
+                                analysis.current["must"].add(name)
+                                analysis.current["may"].add(name)
 
-                    analysis.original["must"].add(name)
-                    analysis.original["may"].add(name)
-            if "assign_to" in label:
-                print("###############")
-                print(successors)
-                for successor in successors:
-                    print((successor, data[successor]))
-                print("###############")
+                                analysis.added["may"].add(name)
+                    print("&&&&&&&&&&&&&&&")
 
+        # for successor in successors:
+        #    print((successor, data[successor]))
         print("***********")
     with open(result_file_name, 'w') as writer:
         writer.write(pprint.pformat(str(analysis)))
